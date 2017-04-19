d78c80e4c552f3658b27659839c88d0265caa754
RadekPC <nowy_xx@o2.pl>
2017-04-19 20:14:54 +0200

    added jquery styles and menu

---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+var gulp = require("gulp"),
+    
+    autoprefixer = require("gulp-autoprefixer"),
+    plumber = require("gulp-plumber"),
+    browserSync = require("browser-sync"),
+    del = require("del"),
+    useref = require("gulp-useref"),
+    uglify = require("gulp-uglify"),
+    gulpif = require("gulp-if"),
+    imagemin = require("gulp-imagemin"),
+    runSequence= require("run-sequence");
+
+/*####################################
+KOMPILACJA SASS DO CSS
+1. Pobierz pluginn gulp-sass
+npm install gulp-sass --save-dev
+PLUMBER umożliwia obsługę błędu tzn. w razie np braku poprawności pliku nie zakańcza działania procesu co umożliwia poprawienie pliku i prawidłowe wykonanie się procesu
+
+*/
+
+
+/*#####################################
+AUTOPREFIXER
+gulp-autoprefixer
+npm install -save-dev gulp-autoprefixer
+*/
+
+
+/*####################################
+Browser SYNC
+npm install browser-sync --save-dev
+*/
+gulp.task("server", function() {
+    browserSync.init({
+        server: "src/"
+    });
+});
+   
+
+/*####################################
+NASŁUCHIWANIE NA ZMIANY
+*/
+gulp.task("watch", function() {
+
+    gulp.watch("src/sass/**/*.scss",["css"]);
+    gulp.watch(["src/*.html","src/**/*.js"],browserSync.reload);
+    
+
+});
+
+
+/*####################################
+narzędzie do czyszczenia katalogów
+npm install --save-dev gulp-useref
+należy dodawać tagi w pliku w którym są źródła do połączenia 
+aby poprawnie wykonać łączenia nalezy najpier wyczyścić katalog3
+######################################
+mimifikacja 
+npm install --save-dev gulp-uglify
+*/
+
+gulp.task("clean", function() {
+   return del("dist/");//usuwa folder z zawartością
+});
+
+
+gulp.task("html",function() {
+    gulp.src("src/*.html")
+        .pipe(useref())
+        .pipe(gulpif("*.js",uglify())  )
+        .pipe(gulp.dest("dist/"));
+
+});
+/*####################################
+IMAGEMIN
+npm install --save-dev gulp-imagemin
+
+
+*/
+gulp.task("images", function() {
+   return gulp.src ("dist/images/*",{
+    base: "dist"
+    
+    })
+    .pipe(gulp.dest("dist/"))
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/"));
+
+});
+
+gulp.task("copy",function() {
+  return  gulp.src(["src/css/**/*.css", "src/images/*", "src/uploads/*"],{
+    base: "src"
+    })
+    .pipe(gulp.dest("dist/"));
+});
+
+/*####################################
+Uruchanianie sekwencyjne
+npm install--save-dev run-sequence
+ZACHOWUJE ASYNCHRONICZNOŚĆ B WAŻNE
+build server pozwala uruchomić gotowa app
+*/
+
+
+gulp.task("build", function() {
+runSequence("clean", "html","copy","images");
+});
+
+ gulp.task("default",["server","watch"]);
