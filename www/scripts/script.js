36c454b2e7c8ae47357be95bfc5455b2a8a9c51b
RadekPC <nowy_xx@o2.pl>
2017-05-10 16:20:47 +0200

    Some little changes, fixed bug with disappering input

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,24 +1,19 @@
 
 $(document).on("pageshow", "#map-page", function () {
+    var input = document.getElementById('searchonmap');
+    input.value = 'Kraków,';
     var defaultLatLng = new google.maps.LatLng(50.0685685, 19.955005);
     if (navigator.geolocation) {
         function success(pos) {
             // location found - show map with these coordinates
-
             drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-
-
         }
-
         function fail(error) {
             drawMap(defaultLatLng);  // failed - show default map
         }
-
         // find the user's current position; cache the location for 5 minutes, timeout after 6 seconds
-        navigator.geolocation.getCurrentPosition(success, fail, { maximumAge: 500000, enableHighAccuracy: true, timeout: 6000 });
-
+        navigator.geolocation.getCurrentPosition(success, fail, { maximumAge: 500000, enableHighAccuracy: true, timeout: 8000 });
     } else {
-        // drawMap(defaultLatLng);  // no geolocation support - show default map
         if (navigator.geolocation) {
             function success(pos) {
                 // location found - show map with these coordinates
@@ -32,7 +27,7 @@ $(document).on("pageshow", "#map-page", function () {
             }
 
             // find the user's current position; cache the location for 
-            // 5 minutes, timeout after 6 seconds
+            // 5 minutes, timeout after 8 seconds
             navigator.geolocation.getCurrentPosition(success,
                 fail,
                 {
@@ -41,11 +36,9 @@ $(document).on("pageshow", "#map-page", function () {
                     timeout: 6000
                 });
         } else {
-            drawMap(defaultLatLng);  // no geolocation support
+            drawMap(defaultLatLng);  // no geolocation support 
         }
-
     }
-
 
     function drawMap(latlng) {
         var markers = [];
@@ -54,15 +47,8 @@ $(document).on("pageshow", "#map-page", function () {
             center: latlng,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-
-
         var map = new google.maps.Map(document.getElementById("map-area"), myOptions);
-
-        var input = document.getElementById('searchonmap');
-        input instanceof window.HTMLTextAreaElement;
-        // add a start marker
-
-        addMarker(latlng);
+        addMarker(latlng);//start marker your pos
 
         console.log(markers[0].getPosition().lat(), markers[0].getPosition().lng());
         var mlat = markers[0].getPosition().lat(),
@@ -71,7 +57,6 @@ $(document).on("pageshow", "#map-page", function () {
         google.maps.event.trigger(map, 'resize');
         map.setZoom(map.getZoom());
 
-
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
         var autocomplete = new google.maps.places.Autocomplete(input);
@@ -79,8 +64,7 @@ $(document).on("pageshow", "#map-page", function () {
 
         var infowindow = new google.maps.InfoWindow();
 
-
-        // Adds a marker to the map and push to the array.
+        // Adds a marker to the map and push to the array (markers).
         function addMarker(location) {
             var marker = new google.maps.Marker({
                 position: location,
@@ -124,7 +108,7 @@ $(document).on("pageshow", "#map-page", function () {
             infowindow.close();
             var place = autocomplete.getPlace();
             if (!place.geometry) {
-                window.alert("Autocomplete's returned place contains no geometry");
+                window.alert("Nie znaleziono takiego miejsca!");// error when autocomplete's returned place contains no geometry
                 return;
             }
 
@@ -135,17 +119,13 @@ $(document).on("pageshow", "#map-page", function () {
                 map.setCenter(place.geometry.location);
                 map.setZoom(17);
             }
-
-            // markerb.setPosition(place.geometry.location);
             addMarker(place.geometry.location);
 
             console.log(place.geometry.location.lat(), place.geometry.location.lng());
-            // markerb.setVisible(true);
-
             pointA = new google.maps.LatLng(mlat, mlong);
             pointB = new google.maps.LatLng(place.geometry.location.lat(), place.geometry.location.lng());
 
-
+            //call of function to display a route
             calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB);
 
 
@@ -161,7 +141,7 @@ $(document).on("pageshow", "#map-page", function () {
                     if (status == google.maps.DirectionsStatus.OK) {
                         directionsDisplay.setDirections(response);
                     } else {
-                        window.alert('Directions request failed due to ' + status);
+                        window.alert('Nie udało się wyznaczyć trasy, błąd: ' + status);// onerror
                     }
                 });
             }
@@ -176,7 +156,6 @@ $(document).on("pageshow", "#map-page", function () {
             }
 
             infowindow.setContent('<div class="info"><strong>' + place.name + '</strong><br>' + address);
-
             infowindow.open(map, markers[1]);
 
         });
